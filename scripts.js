182b868038a14e54ec8fd9315f414bc5af66aa2a
Carlos Eduardo <carloseduardo.and2@gmail.com>
2021-03-25 15:45:45 -0300

    initial commit

---
+++ scripts.js
@@ -0,0 +1,71 @@
+const cells = document.querySelectorAll('.cell')
+const title = document.querySelector('.title')
+const button = document.querySelector('#gameButton')
+
+let board = ['', '', '', '', '', '', '', '', '']
+let counter = 0
+let gameWin = false
+
+// Data cell attributes
+for (let i of cells) {
+    const cell = i.getAttribute('data-cell')
+
+    i.addEventListener('click', () => {  
+        if (gameWin == false) {
+            if (board[cell] != '') {
+                alert('Cell already selected')
+            } else if (counter%2 == 0) {
+                board[cell] = i.innerHTML = 'X'
+                counter++ 
+            } else {
+                board[cell] = i.innerHTML = 'O'
+                counter++ 
+            }
+        }
+        console.log(board)
+        gameWinner()
+    })
+}
+
+// Game winner
+function gameWinner() {
+    for (let i=0; i<=7; i++) {
+        const winningConditions = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ]
+
+        const winCondition = winningConditions[i]
+        let a = board[winCondition[0]]
+        let b = board[winCondition[1]]
+        let c = board[winCondition[2]]
+
+        if (a === '' || b === '' || c === '') {
+            continue
+        }
+        if (a === b && b === c) {
+            gameWin = true
+            button.innerHTML = 'Play Again'
+            const playerWinner = a == 'X' ? 1 : 2
+            title.innerHTML = `Player ${playerWinner} wins! 🎉`
+        }
+    }
+}
+
+// Restart game
+button.addEventListener('click', () => {
+    button.innerHTML = 'Restart Game'
+    title.innerHTML = 'Tic Tac Toe'
+    for (i=0; i<=9; i++) {
+        if (cells[i] != undefined) {
+            cells[i].innerHTML = ''
+        }
+    }
+    board = ['', '', '', '', '', '', '', '', '']
+})
